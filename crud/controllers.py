e0d913fefbf0f2dde622eca9fb5c357aaa68a790
SYED-HAIDER-ALI <s.haider0303@gmail.com>
2023-10-25 12:53:34 +0500

    .

--- crud/controllers.py
+++ crud/controllers.py
@@ -29,6 +29,35 @@ class CRUDController:
             return create_response({'error': str(e)}, SOMETHING_WENT_WRONG, 500)
 
     def update(self, request):
-        pass
+        try:
+            if "id" in request.data:
+                instance = self.serializer_class.Meta.model.objects.filter(id=request.data['id']).first()
+                if instance:
+                    serialized_data = self.serializer_class(instance, data=request.data, partial=True)
+                    if serialized_data.is_valid():
+                        response_data = serialized_data.save()
+                        return create_response(self.serializer_class(response_data).data, SUCCESSFUL, 200)
+                    else:
+                        return create_response({}, get_first_error_message_from_serializer_errors(serialized_data.errors, UNSUCCESSFUL),400)
+                else:
+                    return create_response({}, NOT_FOUND, 404)
+            else:
+                return create_response({}, ID_NOT_PROVIDED, 400)
+
+        except Exception as e:
+            return create_response({'error': str(e)}, SOMETHING_WENT_WRONG, 500)
+
     def delete(self, request):
-        pass
+        try:
+            if "id" in request.data:
+                instance = self.serializer_class.Meta.model.objects.filter(id=request.data['id']).first()
+                if instance:
+                    instance.delete()
+                    return create_response({}, SUCCESSFUL, 200)
+                else:
+                    return create_response({}, NOT_FOUND, 404)
+            else:
+                return create_response({}, ID_NOT_PROVIDED, 400)
+
+        except Exception as e:
+            return create_response({'error':str(e)}, UNSUCCESSFUL, 500)
